ea19c238b4215d7579db55659e6101c961998eb3
mauraisa <aaron.maurais@bc.edu>
2019-04-04 13:11:03 -0400

    More polished version of program. Added tsv input.

--- protein_loc_scraper/locScraper.py
+++ protein_loc_scraper/locScraper.py
@@ -1,36 +1,68 @@
 
 import argparse
+import csv
 import pandas as pd
+import os
+import sys
 
 #my modules
 import scraper
 
 def getArgs():
-    parser = argparse.ArgumentParser(description='Get ')
+    parser = argparse.ArgumentParser(description='Get subcellular location annotations for a list of uniprot protein IDs.')
 
     parser.add_argument('-i', '--idCol', default = 'ID', type = str,
                         help = 'Name of column containing Uniprot IDs.')
 
+    parser.add_argument('-l', '--locCol', default = 'subcellular_loc',
+                        help = 'Name of new column to add with subcellular location.')
+
     parser.add_argument('--nThread', default = None, type = int,
-                        help = 'Number of threads to use to lookup uniprot anotations. '
+                        help = 'Number of threads to use to lookup uniprot annotations. '
                                'Default is the number of logical cores on your system.')
 
-    parser.add_argument('ids', nargs='+')
+    parser.add_argument('-o', '--ofname', type = str, default = None,
+                        help = 'Name of output file. Default is <input_file>_loc.tsv. '
+                               'If multiple input files are given, this argument is ignored.')
+
+    parser.add_argument('--inPlace', default = False, action = 'store_true',
+                        help = 'Overwrite input files with output files. '
+                               'This option overrides the --ofname option.')
+
+    parser.add_argument('input_file', nargs='+',
+                        help = '.tsv or .csv files to process.')
 
     args = parser.parse_args()
-    return args
+
+    #manually processing of certian args
+    ofnames = list()
+    if args.inPlace:
+        ofnames = args.ifnames
+    else:
+        for fname in args.input_file:
+            ofnames.append('{}_loc.tsv'.format(os.path.splitext(fname)[0]))
+        if args.ofname is not None and len(args.input_file) > 1:
+            ofnames = [args.ofname]
+
+    return args, ofnames
+
 
 def main():
-    args = getArgs()
+    args, ofnames = getArgs()
 
-    locations = scraper.getLocList(args.ids, nThread = args.nThread)
+    for i, ifname in enumerate(args.input_file):
+        sys.stdout.write('Working on {}...\n'.format(ifname))
+        inF = open(ifname, 'r')
+        delim = csv.Sniffer().sniff(inF.read(1024)).delimiter
+        df = pd.read_csv(ifname, sep = delim)
 
-    df = pd.DataFrame(data = {'ID': args.ids, 'location': locations})
+        ids = df[args.idCol].tolist()
+        locations = scraper.getLocList(ids, nThread = args.nThread)
 
-    #for i, row in df.iterrows():
-    #   row['location'] = scraper.getLocs(row['ID'])
+        df[args.locCol] = locations
 
-    df.to_csv('../testFiles/temp.tsv', sep = '\t', index = False)
+        df.to_csv(ofnames[i], sep = '\t', index = False)
+        sys.stdout.write('Results written to {}\n'.format(ofnames[i]))
 
 if __name__ == '__main__':
     main()
--- protein_loc_scraper/scraper/parallelization.py
+++ protein_loc_scraper/scraper/parallelization.py
@@ -5,10 +5,20 @@ from multiprocessing.pool import ThreadPool as Pool
 from multiprocessing import cpu_count
 from typing import List
 import tqdm
+import sys
 
 from .scraper import getLocs
 
 def getLocList(uniProtIDs: List, nThread: int = None) -> List:
+    '''
+    Given a list of uniprot IDs, return a list in the same order containing uniprot
+    annotations for subcellular localization.
+
+    :param uniProtIDs: uniprot IDs of proteins to look up.
+    :param nThread: Nunber of threads to use
+    :param threadMult: Factor to multiply number of logical cores by to get number of threads to use.
+    :return: List of subcellular locations.
+    '''
 
     #calculate number of threads reqired
     _nThread = int(1)
@@ -17,11 +27,14 @@ def getLocList(uniProtIDs: List, nThread: int = None) -> List:
     if nThread is None:
         _nThread = cpuCount if cpuCount < listLen else listLen
     else:
-        assert(nThread <= cpuCount)
         _nThread = nThread
 
     #lookup locs using thread pool
+    sys.stdout.write('Searching for locations with {} threads...\n'.format(_nThread))
     with Pool(processes=_nThread) as pool:
-        ret = list(tqdm.tqdm(pool.imap(getLocs, uniProtIDs), total = listLen, miniters=1))
+        ret = list(tqdm.tqdm(pool.imap(getLocs, uniProtIDs),
+                             total = listLen,
+                             miniters=1,
+                             file = sys.stdout))
 
     return ret
