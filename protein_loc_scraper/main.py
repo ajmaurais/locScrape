ab60c279dc579b4e60a583fdf71e8a13d2652de4
mauraisa <aaron.maurais@bc.edu>
2019-04-03 20:19:55 -0400

    Reworked parallization to use ThreadPool
     ...instead of manually controlling parallelism.
     Also added tqdm progress bar.

--- protein_loc_scraper/main.py
+++ protein_loc_scraper/main.py
@@ -11,6 +11,10 @@ def getArgs():
     parser.add_argument('-i', '--idCol', default = 'ID', type = str,
                         help = 'Name of column containing Uniprot IDs.')
 
+    parser.add_argument('--nThread', default = None, type = int,
+                        help = 'Number of threads to use to lookup uniprot anotations. '
+                               'Default is the number of logical cores on your system.')
+
     parser.add_argument('ids', nargs='+')
 
     args = parser.parse_args()
@@ -19,15 +23,14 @@ def getArgs():
 def main():
     args = getArgs()
 
-    locations = scraper.getLocList(args.ids, nThread = 1)
-    print(locations)
+    locations = scraper.getLocList(args.ids, nThread = args.nThread)
 
-    #df = pd.DataFrame(data = {'ID': args.ids, 'location': locations})
+    df = pd.DataFrame(data = {'ID': args.ids, 'location': locations})
 
     #for i, row in df.iterrows():
     #   row['location'] = scraper.getLocs(row['ID'])
 
-    #df.to_csv('../testFiles/temp.tsv', sep = '\t', index = False)
+    df.to_csv('../testFiles/temp.tsv', sep = '\t', index = False)
 
 if __name__ == '__main__':
     main()
--- protein_loc_scraper/scraper/parallelization.py
+++ protein_loc_scraper/scraper/parallelization.py
@@ -1,56 +1,17 @@
 
-#from multiprocessing import Queue
-#from multiprocessing import Process as Worker
-
-from queue import Queue
-from threading import Thread as Worker
+#from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool as Pool
 
 from multiprocessing import cpu_count
 from typing import List
-import sys
-import time
+import tqdm
 
-from .atomic_integer import AtomicCounter
 from .scraper import getLocs
 
-def progressBar(progress, barWidth = 50):
-
-    sys.stdout.write('[')
-    pos = int(barWidth * progress)
-
-    for i in range(barWidth):
-        if i < pos:
-            sys.stdout.write('=')
-        elif i == pos:
-            sys.stdout.write('>')
-        else: sys.stdout.write(' ')
-
-    sys.stdout.write('] {}%\r'.format(int(progress * 100)))
-    sys.stdout.flush()
-
-
-def _getID_progress(counter: AtomicCounter, total: int,
-                    sleepTime: float = 1, barWidth: int = 100):
-
-    count = counter.values
-    while(qsize < total):
-        qsize = queue.qsize()
-        time.sleep(sleepTime)
-        progressBar(qsize, barWidth)
-
-
-def _getID_helper(input_queue: Queue, output_queue: Queue, counter: AtomicCounter):
-    while True:
-        if input_queue.empty():
-            break
-        item = input_queue.get()
-        output_queue.put((item, getLocs(item)))
-        counter.increment(1)
-
-
 def getLocList(uniProtIDs: List, nThread: int = None) -> List:
+
     #calculate number of threads reqired
-    _nThread = int()
+    _nThread = int(1)
     listLen = len(uniProtIDs)
     cpuCount = cpu_count()
     if nThread is None:
@@ -59,26 +20,8 @@ def getLocList(uniProtIDs: List, nThread: int = None) -> List:
         assert(nThread <= cpuCount)
         _nThread = nThread
 
-    input_queue = Queue()
-    output_queue = Queue()
-    for id in uniProtIDs:
-        input_queue.put(id)
-
-    counter = AtomicCounter(0)
-    workers = [Worker(target=_getID_helper, args = (input_queue, output_queue)) for _ in range(_nThread)]
-
-    #start progress function
-    workers.append(Worker(target=_getID_progress, args = (output_queue, listLen)))
-
-    for t in workers:
-        t.start()
-
-    for t in workers:
-        t.join()
-
-    ret = list()
-    while not output_queue.empty():
-        result = output_queue.get()
-        ret.append(result)
+    #lookup locs using thread pool
+    with Pool(processes=_nThread) as pool:
+        ret = list(tqdm.tqdm(pool.imap(getLocs, uniProtIDs), total = listLen, miniters=1))
 
     return ret
