d07e1ad61db94ac41b5d4516ca269cd3413ea451
mauraisa <aaron.maurais@bc.edu>
2019-04-11 16:18:37 -0400

    Additional error checking.

--- protein_loc_scraper/scraper/scraper.py
+++ protein_loc_scraper/scraper/scraper.py
@@ -26,13 +26,18 @@ def _concatLocs(locList: List, delim: str = ';') -> str:
 
 def getLocs(uniprotID: str, nRetry = 10) -> Tuple:
     url = 'http://www.uniprot.org/uniprot/' + uniprotID + '.html'
+    response = None
     for _ in range(nRetry):
         try:
             response = requests.get(url)
-        except requests.exceptions.ConnectionError:
+        except(requests.exceptions.ConnectionError,
+               requests.exceptions.ChunkedEncodingError):
             continue
         else:
             break
+
+    if response is None:
+        return 'error_getting_page', 'error_getting_page', 'error_getting_page'
 
     if response.status_code >= 400:
         return 'no_uniprot_records_found', 'no_uniprot_records_found', 'no_uniprot_records_found'
